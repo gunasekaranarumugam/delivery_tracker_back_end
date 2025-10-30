43ad32d0502d5239b0a87285f92c389bd9fbb5a6
irfaanhameed25 <irfaanhameed25@gmail.com>
2025-10-30 19:22:06 +0530

    Update database.py

--- main/database.py
+++ main/database.py
@@ -8,10 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # --- MySQL connection details ---
-db_user = os.environ.get('db_user')
-db_password = os.environ.get('db_password')
-db_host = os.environ.get('db_host')
-db_name = os.environ.get('db_name')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST')
+db_name = os.environ.get('DB_NAME')
 
 # Construct the database URL
 DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
