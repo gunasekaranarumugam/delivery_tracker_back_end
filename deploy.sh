79956215a92ce258b306b74e8545e6ddb98dbd77
deliverytracker04031994 <deliverytracker04031994@gmail.com>
2025-11-10 22:55:32 +0530

    added ec2

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Pulling latest Docker image..."
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 294253399473.dkr.ecr.ap-south-1.amazonaws.com
+
+docker pull 294253399473.dkr.ecr.ap-south-1.amazonaws.com/delivery-tracker-back-end-main:latest
+
+echo "Stopping old container..."
+docker stop delivery-tracker-backend || true
+docker rm delivery-tracker-backend || true
+
+echo "Starting new container..."
+docker run -d --name delivery-tracker-backend -p 8000:8000 294253399473.dkr.ecr.ap-south-1.amazonaws.com/delivery-tracker-back-end-main:latest
+
+echo "Deployment completed successfully."
