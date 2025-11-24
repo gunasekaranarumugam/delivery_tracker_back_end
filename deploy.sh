a836bc5de45f4bd44c2385b751e306efa699af17
Gunasekaran Arumugam <deliverytracker04031994@gmail.com>
2025-11-24 21:48:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker stop delivery-tracker-backend-master || true
 docker rm delivery-tracker-backend-master || true
 
 echo "Starting new container..."
-docker run -d --name delivery-tracker-back-end-main -p 8000:8000 294253399473.dkr.ecr.ap-south-1.amazonaws.com/delivery-tracker-back-end-main:latest
+docker run -d --name delivery-tracker-backend-master -p 8000:8000 294253399473.dkr.ecr.ap-south-1.amazonaws.com/delivery-tracker-back-end-main:latest
 
 echo "Deployment completed successfully."
